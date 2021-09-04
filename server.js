817d2fcbdde6714610299d9730e4532514fb7237
SKIV-2611 <didm.platonov@yandex.ru>
2021-09-04 21:03:16 +0300

    Stable connection to database without any output

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
-const http = require('http')
-const hostname = '127.0.0.1'
-const port = 3000
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
-})
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
-
 const sql = require('mssql');
-const sqlConfig = {
-  server: 'localhost',
+/*const sqlConfig = {
+  server: '(localdb)\\MSSQLLocalDB',
   options: {
     database: 'DBS_gridContext-6a8df6d3-4faa-4384-9f7e-5aa4364bc234',
 	port: 1433,
@@ -20,16 +8,28 @@ const sqlConfig = {
     MultipleActiveResultSets: true
   }
   
-};
+};*/
+const sqlConfig = {
+	server: '(localdb)\\MSSQLLocalDB',
+    database: 'DBS_gridContext-6a8df6d3-4faa-4384-9f7e-5aa4364bc234',
+	port: 1433,
+    options: {
+		trustedConnection: true,
+        encrypt: false,
+		multipleActiveResultSets: true
+    }
+}
 
-(async()=> {
+async() => {
     try {
         //make sure that any items are correctly URL encoded in the connection string
-        await sql.connect(sqlConfig);
-		const result = await sql.query`select * from dbo.PaymentOrder; GO`
+		//await sql.connect('Server=(localdb)\\mssqllocaldb;Database=DBS_gridContext-6a8df6d3-4faa-4384-9f7e-5aa4364bc234;Trusted_Connection=True;MultipleActiveResultSets=true');
+        //await sql.connect('Server=(localdb)\\mssqllocaldb;Database=DBS_gridContext-6a8df6d3-4faa-4384-9f7e-5aa4364bc234;Trusted_Connection=True');
+		await sql.connect(sqlConfig)
+		const result = await sql.query`select * from dbo.PaymentOrder`
 		console.dir(result)
     } catch (err) {
         // ... error checks
-		console.log(err)
+		console.dir(err)
     }
-})();
+}
